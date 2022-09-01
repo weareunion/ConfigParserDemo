a2a6a62850bfced7b4be5d4d01ee3dfeeb7f0e59
karl3@clemson.edu <Karlschmidt01!>
2022-09-01 17:41:27 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,93 @@
+
+# Config Parser Demo
+
+This is a coding demo. The instructions are listed at the bottom of the page.
+
+Written by Karl Schmidt (https://karlschmidt.me/) on September 1st, 2022
+## About the Tech
+
+This project was written, for the most part (excluding the JS embedded in the `demo.html` file), in TypeScript. This 
+is a compiled language, and can be compiled from source. This compiled source (JS) lives in the `/dist` folder. 
+I have already compiled the TypeScript for you, but you can also do it yourself as well. 
+
+To do this, execute the following in the root directory of the project: 
+`npm run build`
+
+**NOTE:**
+This may fail if you do not have TypeScript installed. 
+You can fix this by running 
+```
+npm install typescript
+``` 
+or just 
+```
+npm i
+``` 
+since the package.json makes note of this dependency.
+
+Once you have compiled the source (or have decided to use the included one) you can reload the `demo.html` page to begin. 
+
+### The API
+
+#### Creating an Instance
+Create an instance of the `ConfigurationFile` class, using the following method (you can replace the given text with your own config file):  
+
+```JavaScript
+let configfile = new ConfigurationFile("test_text=true")
+```
+
+Unless you set the `autoProcess` feature to `false` in the constructor, 
+the instance will automatically process and populate its internal map structure. 
+
+#### Handling Errors 
+
+The following errors can be thrown (-> sigifies inheritance):
+
+**`ParseError` -> `ConfigError` -> `Error`**: Throws when: a) there is a line with no assigment (such as "data value"), or there is one with more than one assigment ("data = value1 = value2")
+
+**`DuplicateIndex` -> `ConfigError` -> `Error`**: Throws when there is a duplicate index
+
+I decided to create this pattern since we can individually detect ConfigErrors overarchingly but also ParseError and DuplicateIndex. 
+
+#### Extracting Data
+
+Extracting data can be done with the following two methods
+
+`ConfigurationFile.get(key:string)` and `ConfigurationFile.getAllEntries()`. 
+
+For example, we can get a certain key 
+
+```JavaScript
+let configfile = new ConfigurationFile("test_text=true")
+configfile.get("test_text") //true:boolean
+configfile.get("something_else") //undefined
+```
+
+...or return all entries as one single object 
+
+```JavaScript
+let configfile = new ConfigurationFile("test_text=true")
+configfile.getAllEntries() //{"test_text":true}:{[key:string]:[value:String|Number|Boolean]}
+```
+## Demo
+
+There is a simple HTML site that can demo the JS. This can be accessed as /public/demo.html.
+## Instructions
+
+Please create a parsing tool that takes the example config file (provided below) and turns it into a usable object in the language of your choice (hash, JSON object, associative array, class, etc). The instructions for this are below. Please let us know if you have any questions!
+
+1. Do not use existing "complete" configuration parsing libraries/functions, we want to see how you would write the code to do this.
+
+2. Use of core and stdlib functions/objects such as string manipulation, regular expressions, etc is ok.
+
+3. We should be able to get the values of the config parameters in code, via their name. How this is done specifically is up to you.
+
+4. Boolean-like config values (on/off, yes/no, true/false) should return real booleans: true/false.
+
+5. Numeric config values should return real numerics: integers, doubles, etc
+
+6. Ignore or error out on invalid config lines, your choice.
+
+7. Please include a short example usage of your code so we can see how you call it/etc.
+
+8. Push your work to a public git repository (github, bitbucket, etc) and send us the link.
---
+++ src/ConfigurationFile.ts
@@ -0,0 +1,209 @@
+/**
+ * Written by Karl Schmidt
+ */
+
+/**
+ * Throws when there is a duplicate index in the map
+ * @extends Error
+ */
+class ConfigError extends Error{
+    constructor(props: string) {
+        super("Could not read configuration file. " + props);
+    }
+
+}
+
+/**
+ * Throws when there is a parsing error (general)
+ * @extends ConfigError
+ */
+class ParseError extends ConfigError{
+    constructor(lineIndex:number, line:string, error?:string) {
+        super(`Error on line ${lineIndex} : "${line}"${(error ?? "")}`);
+
+        // Set prototype
+        Object.setPrototypeOf(this, ParseError.prototype);
+    }
+}
+
+/**
+ * Throws when there is a duplicate index in the map
+ * @extends ConfigError
+ */
+class DuplicateIndex extends ConfigError{
+    constructor(line:number, indexName:string) {
+        super(`Duplicate index (${indexName}) on line ${line}.`);
+
+        // Set prototype
+        Object.setPrototypeOf(this, DuplicateIndex.prototype);
+    }
+}
+
+/**
+ * ConfigurationFile Obj takes in a config file string,
+ * and parses it, preserving datatype where needed.
+ */
+class ConfigurationFile {
+    /**
+     * Contains the raw configuration text for
+     * later reference if needed
+     * @private
+     */
+    private _configurationTextRaw;
+    /**
+     * Contains a map of the configuration data,
+     * with the map key = to the configuration key,
+     * and the map value = the configuration value after
+     * processing and setting the datatype.
+     *
+     * Map<string, Boolean|String|Number>
+     * @private
+     */
+    private _map;
+
+    /**
+     * Mapping for boolean equivalents.
+     * @precondition strings to compare are always lowercase and trimmed
+     * @private
+     */
+    private _booleanEquivalents = {
+        true : ["yes", "on", "true", "enabled", "enable"],
+        false: ["no", "off", "false", "disabled", "disable"]
+    }
+
+    /**
+     * Takes config file and optionally starts
+     * the processing
+     * @param configurationText The config info as string
+     * @param autoProcess If the config file should be automatically
+     * processed or not
+     */
+    constructor(configurationText: string, autoProcess: boolean = true) {
+        // Set the config text to the field
+        this._configurationTextRaw = configurationText
+
+        // Instantiate Map
+        this._map = new Map<string, Boolean|String|Number>;
+
+        // If requested, process the data in
+        if (autoProcess) this.process()
+    }
+
+    /**
+     * Retrieve data point from key after config process
+     * @wraps Map.get(key)
+     * @param key
+     * @returns Boolean|String|Number|undefined Returns data point if exists,
+     * undefined if not
+     */
+    get(key:string){
+        return this._map.get(key)
+    }
+
+    /**
+     * Retrieve data point from key after config process
+     * @wraps Map.entries()
+     * @returns IterableIterator<string, Boolean|String|Number> Returns all active entries
+     */
+    getAllEntries(){
+        return this._map.entries()
+    }
+
+    /**
+     * Processes the file
+     */
+    process(){
+
+        // Split lines by new line '\n'
+        let line_split = ConfigurationFile.splitByLines(this._configurationTextRaw)
+
+        // Iterate through every line and process each
+        line_split.forEach((line:string, index:number) => {
+
+            // Trim whitespace on the incoming line
+            line = line.trim()
+
+            // Return (continue) if the line is blank
+            if (line === "") return;
+
+            // Split the line at the '=' sign
+            let splitLine = line.split('=')
+
+            // Trim whitespace on the split lines
+            splitLine[0] = splitLine[0].trim();
+            if (splitLine.length == 2) splitLine[1] = splitLine[1].trim();
+
+            // If there is no '=' sign, detect and validate a comment,
+            // if there are more than 1 '=' sign, throw a ParseError (extends ConfigError)
+            if (splitLine.length == 1){
+
+                // ...then check if the line is formatted as a comment, if so, return (continue), if not,
+                // throw a ParseError (extends ConfigError).
+                if (splitLine[0][0] !== "#"){
+                    throw new ParseError(index, line)
+                }else{
+                    return;
+                }
+
+            }else if (splitLine.length !== 2){
+                // Throw error if there are multiple assignments
+                throw new ParseError(index, line, " Cannot parse chained assignments.")
+            }
+
+            // Check to ensure that the index has not already been set. If it has,
+            // throw a DuplicateIndex (extends ConfigError).
+            if (this._map.has(splitLine[0])){
+                throw new DuplicateIndex(index, splitLine[0])
+            }
+
+            // If all is successful, process the datatype and add an entry into the map
+            this._map.set(splitLine[0], this.processDataType(splitLine[1]))
+        })
+    }
+
+    /**
+     * Map the map to a JS Object
+     */
+    toOBJ(){
+        let Obj:{[key:string]:[value:String|Number|Boolean]} = {};
+
+        // Iterate through map and add to object
+        this._map.forEach((value:any, key) => {
+            Obj[key] = value
+        })
+
+        return Obj
+    }
+
+    /**
+     * Tries to detect the datatype and set it
+     * @param data The data string to be processed
+     * @private
+     */
+    private processDataType(data:string){
+        // Trim data
+        data = data.trim()
+
+        // Check if lowercase data exists in the _booleanEquivalents
+        // lists for true and false. Return true of false if one is in
+        // one of the lists
+        if (this._booleanEquivalents.true.includes(data.toLowerCase())) return true;
+        if (this._booleanEquivalents.false.includes(data.toLowerCase())) return false;
+
+        // Determine if the data is a number
+        let asNumber = Number(data);
+
+        // If so, return as a number, if not, return as a string
+        return (isNaN(+asNumber) ? data : asNumber)
+    }
+
+    /**
+     * Split the line by new line (\n)
+     * @param input the input string
+     * @private
+     */
+    private static splitByLines(input:string){
+        return input.split(/\r?\n/)
+    }
+
+}
